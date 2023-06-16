d791e541ea933afceb1b1e7e565663e28c4bb186
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-06-16 14:23:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function createTable() {
     //Write your code here
-  
+	const row = prompt('Input number of rows');
+	const col = prompt('Input number of columns');
+
+	const table = document.createElement('table');
+	
+	for(let i = 0; i < row.length; i++){
+		const row = document.createElement('tr');
+		for(let j = 0; j < col.length; j++){
+			const col = document.createElement('td');
+			col.innerText = `Row-${i} Column-${j}`;
+			row.append(col);
+		}
+		table.appendChild(row);
+	}
 }
